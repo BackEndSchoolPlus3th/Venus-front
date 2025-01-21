416a38f04cfaadb8de03ab82b161e9d52de32be6
sky0427 <sukeun95@gmail.com>
2025-01-21 17:29:03 +0900

    next.config.ts 파일 수정

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  webpack(config) {
+  reactStrictMode: true,
+  webpack5: (config: any, { isServer }: any) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack']
     })
+    return config
   }
 }
 
